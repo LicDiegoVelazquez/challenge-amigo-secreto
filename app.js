526d0af65d3bd6d8923bfb6bc585e5bd2489b419
LicDiegoVelazquez <licdiegovelazquez@gmail.com>
2025-08-24 21:14:43 -0300

    Agregaro funcion para guardar nombre de un amigo en un array

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+
+//funcion para guardar el nombre del amigo
+function agregarAmigo(){
+    let nombreAmigo = document.getElementById('amigo').value; //llama al elemento de html por el id y toma el valor
+    if (nombreAmigo == ''){ //crea un alerata si no se introduce un nombre
+        alert('Debes añadir un nombre');
+    } else {
+    listaAmigos.push(nombreAmigo); //añade el nombre al array
+    console.log(listaAmigos);//muestra el array en la consola
+    document.getElementById('amigo').value=""; //vacia la caja
+    } 
+}
+
+//funcion asigna elementos a la lista
+function asignarLista(){
+    let elementoHtml = document.getElementById('listaAmigos');
+    elementoHtml.innerHTML = '';
+    return;
+}
